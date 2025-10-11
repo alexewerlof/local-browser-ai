b5bf02f66cf8bad47ac25d58a415a15b153823b0
Alex Ewerlöf <hanifbbz@gmail.com>
2025-10-11 17:59:09 +0200

    experimenting with the wrapper

--- util/dom.js
+++ util/dom.js
@@ -40,3 +40,69 @@ export function h(tagName, attributes, ...children) {
     }
     return el;
 }
+
+export class Wrapper {
+    constructor(ref) {
+        if (typeof ref !== 'string' && !(ref instanceof HTMLElement)) {
+            throw new TypeError(`Expected an id or HTML Element. Got ${ref} (${typeof ref})`)
+        }
+        this.ref = ref
+    }
+
+    get el() {
+        return typeof this.ref === 'string' ? document.getElementById(this.ref) : this.ref
+    }
+
+    get val() {
+        return this.el.value
+    }
+
+    set val(value) {
+        this.el.value = value
+        return this
+    }
+
+    attr(name, value) {
+        this.el[name] = value
+        return this
+    }
+
+    on(eventName, handler) {
+        this.el.addEventListener(eventName, handler)
+        return this
+    }
+
+    click(handler) {
+        return this.on('click', handler)
+    }
+
+    off(eventName, handler) {
+        this.el.removeEventListener(eventName, handler)
+        return this
+    }
+
+    hide() {
+        return this.attr('hidden', true)
+    }
+
+    show() {
+        return this.attr('hidden', false)
+    }
+
+    disable() {
+        return this.attr('disabled', true)
+    }
+
+    enable() {
+        return this.attr('disabled', false)
+    }
+
+    focus() {
+        this.el.focus()
+        return this
+    }
+
+    text() {
+        return this.attr('innerText', value)
+    }
+}
